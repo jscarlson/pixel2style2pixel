f6af4f1f1c76ae602bc684ae11e597db4aa8753c
jscarlson <jcarlson238@gmail.com>
2021-08-27 12:41:09 -0400

    Debug

--- training/coach.py
+++ training/coach.py
@@ -87,6 +87,7 @@ class Coach:
 			for batch_idx, batch in enumerate(self.train_dataloader):
 				self.optimizer.zero_grad()
 				x, y = batch
+				print(x.size(), y.size())
 				x, y = x.to(self.device).float(), y.to(self.device).float()
 				y_hat, latent = self.net.forward(x, return_latents=True)
 				loss, loss_dict, id_logs = self.calc_loss(x, y, y_hat, latent)
