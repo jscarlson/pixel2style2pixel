3463bec2e85d3d0ffb1295828064a658e0d61138
jscarlson <jcarlson238@gmail.com>
2021-09-14 00:43:14 -0400

    Debug wandb logging

--- utils/wandb_utils.py
+++ utils/wandb_utils.py
@@ -23,25 +23,27 @@ class WBLogger:
         wandb.log(log_dict)
 
     @staticmethod
-    def log_dataset_wandb(dataset, dataset_name, n_images=16):
+    def log_dataset_wandb(dataset, dataset_name, n_images=8):
         idxs = np.random.choice(a=range(len(dataset)), size=n_images, replace=False)
         data = [wandb.Image(dataset.source_paths[idx]) for idx in idxs]
         wandb.log({f"{dataset_name} Data Samples": data})
 
     @staticmethod
     def log_images_to_wandb(x, y, y_hat, id_logs, prefix, step, opts):
-        im_data = []
-        column_names = ["Source", "Target", "Output"]
-        if id_logs is not None:
-            column_names.append("ID Diff Output to Target")
-        for i in range(len(x)):
-            cur_im_data = [
-                wandb.Image(common.log_input_image(x[i], opts)),
-                wandb.Image(common.tensor2im(y[i])),
-                wandb.Image(common.tensor2im(y_hat[i])),
-            ]
+        if step % 500 == 0:
+            print(step, end="****\n")
+            im_data = []
+            column_names = ["Source", "Target", "Output"]
             if id_logs is not None:
-                cur_im_data.append(id_logs[i]["diff_target"])
-            im_data.append(cur_im_data)
-        outputs_table = wandb.Table(data=im_data, columns=column_names)
-        wandb.log({f"{prefix.title()} Step {step} Output Samples": outputs_table})
+                column_names.append("ID Diff Output to Target")
+            for i in range(len(x)):
+                cur_im_data = [
+                    wandb.Image(common.log_input_image(x[i], opts)),
+                    wandb.Image(common.tensor2im(y[i])),
+                    wandb.Image(common.tensor2im(y_hat[i])),
+                ]
+                if id_logs is not None:
+                    cur_im_data.append(id_logs[i]["diff_target"])
+                im_data.append(cur_im_data)
+            outputs_table = wandb.Table(data=im_data, columns=column_names)
+            wandb.log({f"{prefix.title()} Step {step} Output Samples": outputs_table})
